7d32aa7a1597c5477de17083431637fdf7ff6612
Martin Petrov <martin_petrov@me.com>
2024-02-29 15:19:26 +0200

    chore: remove 'cache-forever' response leftovers

--- utils/consts.ts
+++ utils/consts.ts
@@ -4,8 +4,6 @@ export const CAN_EMAIL_UNVERIFIED_IDENTITIES = false;
 // disabled until Sharp works in Deno or ImageMagick doesn cause CPU time error on Deno Deploy
 export const RESIZE_PHOTOS_IN_DENO = false;
 
-export const CACHE_FOREVER_PARAM = "cache_forever";
-
 export const ASSETS_EXTENSIONS = [
   ".css$",
   ".js$",
