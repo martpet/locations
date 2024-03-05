937b631cc6a832317937f6907a3a52fd3b24e698
Martin Petrov <martin_petrov@me.com>
2024-03-05 10:21:55 +0200

    improve: change Edit link wording

--- routes/places/(_components)/PlaceArticle.tsx
+++ routes/places/(_components)/PlaceArticle.tsx
@@ -73,7 +73,7 @@ export default function PlaceArticle(props: PlaceProps) {
         )}
         {props.editLink && (
           <p>
-            <a href={props.editLink}>Редактирай</a>
+            <a href={props.editLink}>Редактирай страницата</a>
           </p>
         )}
       </footer>
